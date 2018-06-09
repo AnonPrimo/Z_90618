f18d8dc7e968266ce4c582af752465fdbc2e0638
jul-ia <julie78sd@gmail.com>
2018-06-09 20:50:49 +0300

    Добавьте файлы проекта.

---
+++ Z3(2)/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//
+namespace Z3_2_
+{
+    class MyClass
+    {
+        public string change;
+    }
+
+    struct MyStruct
+    {
+        public string change;
+
+    }
+
+    class Program
+    {
+
+        public static void ClassTaker(MyClass myClass)
+        {
+            myClass.change = "changed";
+        }
+
+        public static void StructTaker(MyStruct myStruct)
+        {
+            myStruct.change = "changed";
+        }
+
+        static void Main(string[] args)
+        {
+            MyClass c = new MyClass();
+            Console.WriteLine("----- Class ----");
+            c.change = "not changed";
+            Console.WriteLine(c.change);
+            ClassTaker(c);
+            Console.WriteLine(c.change);
+
+            MyStruct s = new MyStruct();
+            Console.WriteLine("----- Struct ----");
+            s.change = "not changed";
+            Console.WriteLine(s.change);
+            StructTaker(s);
+            Console.WriteLine(s.change);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Z_90618/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z_90618
+{
+
+    struct Train
+    {
+        string city;
+        int num;
+        int hh;
+        int mm;
+
+        public void FillIn(string t, int n, int h, int m)
+        {
+            city = t;
+            num = n;
+            hh = h;
+            mm = m;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("---------- Train -----------");
+            Console.WriteLine("Destination \t|" + city);
+            Console.WriteLine("Number of tain \t|" + num);
+            Console.WriteLine("Departure time \t|" + hh + ":" + mm);
+            Console.WriteLine();
+        }
+
+        public bool F(int n)
+        {
+            if (n == num) return true;
+            else return false;
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Train[] t = new Train[3];
+            string c;
+            int n, h, m;
+            for(int i=0; i<3; i++)
+            {
+                Console.WriteLine("-----------------");
+                Console.Write("Input destination: ");
+                c = Console.ReadLine();
+                Console.Write("Input number of tain: ");
+                n = int.Parse(Console.ReadLine());
+                Console.Write("Input departure time: (hours): ");
+                h = int.Parse(Console.ReadLine());
+                Console.Write("  (minutes): ");
+                m = int.Parse(Console.ReadLine());
+                t[i].FillIn(c, n, h, m);
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("--------- Find train ----------");
+            Console.Write("Input num of train: ");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            for(int i=0; i<3; ++i)
+            {
+                if (t[i].F(n)) t[i].Show();
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
